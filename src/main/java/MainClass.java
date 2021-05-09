edc3d2845886bd689071bf82a231b6d913bf3386
jecky <jecky.toledo@microfocus.com>
2021-05-09 15:02:25 +0300

    Update MainClass.java
    update with another change to main

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,6 +1,7 @@
 public class MainClass {
     public static void main(String[] args) {
         System.out.println(" my complete hello world");
-        System.out.println("a small change");
+        System.out.println(" a small change");
+        System.out.println(" another change to the manin class");
     }
 }
